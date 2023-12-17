772c81e9a834c799f82b270f6e72acb5c760fb92
Alok Singh <mailbox.alok.singh.11@gmail.com>
2023-12-17 13:52:42 +0530

    Trapping Rainwater Problem Added

--- ArraysCC.java
+++ ArraysCC.java
@@ -121,7 +121,36 @@ public class ArraysCC{
         }
         System.out.println("MAX SUM: "+ms);
     }
+    // Trapping Rainwater
+    public static int trappingRainWater(int height[]){
+        int n = height.length;
+
+        // left max boundary - array
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1;i<n;i++){
+            leftMax[i] = Math.max(height[i],leftMax[i-1]);
+        }
+
+        // right max boundary - array
+        int rightMax[] = new int[n];
+        rightMax[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] = Math.max(height[i],rightMax[i+1]);
+        }
+
+        int trappedWater = 0;
+        for(int i=0;i<n;i++){
+            // water level = min(leftmax,rightmax)
+            int waterLevel = Math.min(rightMax[i],leftMax[i]);
+            // trapped water = (waterlevel - height) * width) [HERE, Width=1]
+            trappedWater += (waterLevel-height[i]);
+        }
+        return trappedWater;
+    }
+    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        
     }
 }
