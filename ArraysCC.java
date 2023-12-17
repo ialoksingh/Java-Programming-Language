aeb0d3bcde0a370b0402ee425335a8bdd400492b
Alok Singh <mailbox.alok.singh.11@gmail.com>
2023-12-17 14:25:10 +0530

    Buy and Sell stock added

--- ArraysCC.java
+++ ArraysCC.java
@@ -148,9 +148,21 @@ public class ArraysCC{
         }
         return trappedWater;
     }
-    
+    // Buy and Sell Stocks
+    public static int buyAndSellStocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0;i<prices.length;i++){
+            if(buyPrice<prices[i]){
+                int profit = prices[i]-buyPrice;
+                maxProfit = Math.max(profit,maxProfit);
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
     }
 }
