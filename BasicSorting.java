d2e32b56839f8672b47c6a7b57cbf98b016d75fa
Alok Singh <mailbox.alok.singh.11@gmail.com>
2023-12-25 19:29:31 +0530

    Descending Order Code Added

--- BasicSorting.java
+++ BasicSorting.java
@@ -76,6 +76,65 @@ public class BasicSorting{
             }
         }
     }
+    // *********************DESCENDING ORDER******************
+    // // Bubble Sort
+    // public static void bubbleSort(int arr[]){
+    //     for(int i=0;i<arr.length-1;i++){
+    //         for(int j=0;j<arr.length-1-i;j++){
+    //             if(arr[j+1]>arr[j]){
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j+1];
+    //                 arr[j+1] = temp;
+    //             }
+    //         }
+    //     }
+    // }
+    // // Selection Sort
+    // public static void selectionSort(int arr[]){
+    //     for(int i=0;i<arr.length-1;i++){
+    //         int maxPos = i; 
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if(arr[j]>arr[maxPos]){
+    //                 maxPos = j;
+    //             }
+    //         }
+    //         // swap
+    //         int temp = arr[i];
+    //         arr[i] = arr[maxPos];
+    //         arr[maxPos] = temp;
+    //     }
+    // }
+    // // Insertion Sort
+    // public static void insertionSort(int arr[]){
+    //     for(int i=1;i<arr.length;i++){
+    //         int curr = arr[i];
+    //         int prev = i-1;
+    //         while(prev>=0 && arr[prev]<curr){
+    //             arr[prev+1] = arr[prev];
+    //             prev--;
+    //         }
+    //         arr[prev+1] = curr;
+    //     }
+    // }
+    // // Counting Sort
+    // public static void countingSort(int arr[]){
+    //     int max = Integer.MIN_VALUE;
+    //     for(int i=0;i<arr.length;i++){
+    //         max = Math.max(max,arr[i]);
+    //     }
+    //     int count[] = new int[max+1];
+    //     for(int i=0;i<arr.length;i++){
+    //         count[arr[i]]++;
+    //     }
+    //     int j=0;
+    //     for(int i=count.length-1;i>=0;i--){
+    //         while(count[i]>0){
+    //             arr[j] = i;
+    //             j++;
+    //             count[i]--;
+    //         }
+    //     }
+    // }
     // Main Function
     public static void main(String args[]){
         int arr[] = {1,4,1,3,2,4,3,7};
