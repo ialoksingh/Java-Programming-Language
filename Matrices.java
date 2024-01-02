e57b2734378100aef542543d81564400632a5083
Alok Singh <mailbox.alok.singh.11@gmail.com>
2024-01-02 18:08:07 +0530

    Search in Sorted Matrix Added

--- Matrices.java
+++ Matrices.java
@@ -56,8 +56,25 @@ public class Matrices{
         }
         System.out.println("Diagonal Sum: "+sum);
     }
+    // Search in a sorted(row-wise as well as column-wise) matrix
+    public static boolean staircaseSearch(int matrix[][],int key){
+        int row = matrix.length-1, col = 0;
+        while(row>=0 && col<matrix[0].length){
+            if(matrix[row][col]==key){
+                System.out.println("Key found at index ("+row+","+col+")");
+                return true;
+            }else if(key>matrix[row][col]){
+                col++;
+            }else{
+                row--;
+            }
+        }
+        System.out.println("Key Not Found!");
+        return false;
+    }
     public static void main(String args[]){
-        int matrix[][] = {{1,2,3,},{4,5,6},{7,8,9}};
-        diagonalSum(matrix);
+        int matrix[][] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+        int key = 33;
+        staircaseSearch(matrix,key);
     }
 }
