992c07bf25ec94dc563ec4362b18dbf5add61ef9
Alok Singh <mailbox.alok.singh.11@gmail.com>
2024-01-02 17:06:26 +0530

    Diagonal Sum Added

--- Matrices.java
+++ Matrices.java
@@ -31,8 +31,33 @@ public class Matrices{
             endCol--;
         }
     }
+    // Diagonal Sum
+    public static void diagonalSum(int matrix[][]){
+        int sum = 0;
+
+        // Brute Force Approach
+        // for(int i=0;i<matrix.length;i++){
+        //     sum+=matrix[i][i];
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if((i+j)==matrix.length-1 && (i!=j)){
+        //             sum+=matrix[i][j];
+        //         }
+        //     }
+        // }
+        
+        // Optimised Approach
+        for(int i=0;i<matrix.length;i++){
+            // Primary Diagonal
+            sum+=matrix[i][i];
+            // Secondary Diagonal
+            if(i!=matrix.length-1-i){
+                sum+=matrix[i][matrix.length-1-i];
+            }
+        }
+        System.out.println("Diagonal Sum: "+sum);
+    }
     public static void main(String args[]){
-        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        spiralMatrix(matrix);
+        int matrix[][] = {{1,2,3,},{4,5,6},{7,8,9}};
+        diagonalSum(matrix);
     }
 }
