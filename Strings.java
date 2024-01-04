9efab4ade20dc9be91e08be62cec7657fb29fdef
Alok Singh <mailbox.alok.singh.11@gmail.com>
2024-01-04 13:07:50 +0530

    Shortest Path Problem Added

--- Strings.java
+++ Strings.java
@@ -9,8 +9,26 @@ public class Strings{
         }
         return true;
     }
+    // Get Shortest Path (directions)
+    public static float getShortestPath(String str){
+        int x = 0, y = 0;
+        for(int i=0;i<str.length();i++){
+            char dir = str.charAt(i);
+            if(dir=='E'){
+                x++;
+            }else if(dir=='W'){
+                x--;
+            }else if(dir=='N'){
+                y++;
+            }else{
+                y--;
+            }
+        }
+        int x2 = x*x, y2 = y*y;
+        return (float)Math.sqrt(x2+y2);
+    }
     public static void main(String args[]){
-        String str = "racecar";
-        System.out.println(isPalindrome(str));
+        String str = "WNEENESENNN";
+        System.out.println(getShortestPath(str));
     }
 }
