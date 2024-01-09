84a0da61eda9315a9288174ba8067310808f10b4
Alok Singh <mailbox.alok.singh.11@gmail.com>
2024-01-09 17:01:07 +0530

    toUpperCase Added

--- Strings.java
+++ Strings.java
@@ -37,8 +37,23 @@ public class Strings{
         }
         System.out.println(largest);
     }
+    // Convert first letter of every word in a string to uppercase
+    public static void toUpperCase(String str){
+        StringBuilder sb = new StringBuilder("");
+        sb.append(Character.toUpperCase(str.charAt(0)));
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' '&& i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+        System.out.println(sb);
+    }
     public static void main(String args[]){
-        String str[] = {"apple", "Mango", "guava"};
-        printLargestString(str);
+        String str = "hello, i am alok.";
+        toUpperCase(str);
     }
 }
