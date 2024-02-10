7771de5f370d7244c4c541fe7d471231c3926def
Alok Singh <mailbox.alok.singh.11@gmail.com>
2024-02-10 21:24:53 +0530

    updateIthBit

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,15 +1,49 @@
-public class BitManipulation{
+/**
+ * BitManipulation
+ */
+public class BitManipulation {
+    // Odd or Even
     public static void oddOrEven(int n){
         int bitMask = 1;
-        if((n&bitMask)==0){
-            // Even number
-            System.out.println("Even Number");
+        if((n&bitMask)==1){
+            System.out.println("Odd");
         }else{
-            // Odd Number
-            System.out.println("Odd Number");
+            System.out.println("Even");
         }
     }
-    public static void main(String args[]){
+    // Get ith bit
+    public static int getIthBit(int n, int i){
+        int bitMask = 1<<i;
+        if((n&bitMask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    // Set ith bit
+    public static int setIthBit(int n,int i){
+        int bitMask = 1<<i;
+        return n|bitMask;
+    }
+    // Clear ith Bit
+    public static int clearIthBit(int n, int i){
+        int bitMask = 1<<i;
+        return n^bitMask;
+    }
+    // Update ith Bit
+    public static int updateIthBit(int n,int i, int newBit){
+        // if(newBit==0){
+        //     return clearIthBit(n, i);
+        // }else{
+        //     return setIthBit(n, i);
+        // }
+        /* one more way */
+        clearIthBit(n, i);
+        int bitMask = newBit<<i;
+        return n|bitMask;
+    }
+    // Main Function
+    public static void main(String[] args) {
         
     }
 }
